1f6dc183cc0746b5bc28390c5d9d2670c0e9f049
ACER\Peter <peter@peter-pokorra.de>
2019-08-18 12:50:11 +0200

    Interface and Classes added

---
+++ FlyWithWings.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DesignPatterns
+{
+    class FlyWithWings : IFlyBehaviour
+    {
+        public void Fly()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ Quack.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DesignPatterns
+{
+    class Quack : IQuackBehaviour
+    {
+        void IQuackBehaviour.Quack()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
